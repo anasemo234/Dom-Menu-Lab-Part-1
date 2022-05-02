76ba23ca7962fd8988531ed872bd6af52b502b3d
Anasemos Kassahun <Anasemosaka@gmail.com>
2022-05-02 00:15:23 -0400

    First commit

--- js/script.js
+++ js/script.js
@@ -1,37 +1,34 @@
 // Task 3.0
 // Menu data structure
 var menuLinks = [
-    {text: 'about', href: '/about'},
-    {text: 'catalog', href: '/catalog'},
-    {text: 'orders', href: '/orders'},
-    {text: 'account', href: '/account'},
-  ];
-
+  { text: "about", href: "/about" },
+  { text: "catalog", href: "/catalog" },
+  { text: "orders", href: "/orders" },
+  { text: "account", href: "/account" },
+];
 
 //   Task 1.0
-const mainEl = document.querySelector('main')
+const mainEl = document.querySelector("main");
 // Task 1.1
-mainEl.style.backgroundColor='#4a4e4d';
+mainEl.style.backgroundColor = "#4a4e4d";
 // Task 1.2
-mainEl.innerHTML =  '<h1>SEI Rocks!</h1>';
+mainEl.innerHTML = "<h1>SEI Rocks!</h1>";
 // Task 1.3
-mainEl.classList.add  ('flex-ctr');
+mainEl.classList.add("flex-ctr");
 // Task 2.0
-const topMenuEl = document.querySelector('#top-menu'); 
+const topMenuEl = document.querySelector("#top-menu");
 // Task 2.1
-document.getElementById('top-menu').style.height = '100%';
+document.getElementById("top-menu").style.height = "100%";
 // Task 2.2
-topMenuEl.style.backgroundColor="var(--sub-menu-bg)";
+topMenuEl.style.backgroundColor = "var(--sub-menu-bg)";
 // Task 2.3
-topMenuEl.classList.add  ('flex-around');
+topMenuEl.classList.add("flex-around");
 
 // Task 3.1
-for(let menuLink of menuLinks){
-    let myLink = document.createElement('a');
-    myLink.setAttribute('href',  menuLink.href);
-    myLink.innerHTML = menuLink.text;
-    topMenuEl.append(myLink);
-    console.log(myLink);
+for (let menuLink of menuLinks) {
+  let myLink = document.createElement("a");
+  myLink.setAttribute("href", menuLink.href);
+  myLink.innerHTML = menuLink.text;
+  topMenuEl.append(myLink);
+  console.log(myLink);
 }
-
-
